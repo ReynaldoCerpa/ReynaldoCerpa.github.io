18ae9f5143b3cf6aa3c68d75cfcd97a4d744b164
ReynaldoCerpa <reynaldocerpa07@gmail.com>
2021-12-11 21:50:13 -0800

    coming soon text

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Landing from './pages/Landing';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" />
+        <Route exact path="/" element={<Landing/>}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+const Landing = () => {
+
+    return (
+        <div style={{display: "flex", justifyContent: "center"}}>
+            <h1>Coming soon...</h1>
+        </div>
+    )
+}
+
+export default Landing
